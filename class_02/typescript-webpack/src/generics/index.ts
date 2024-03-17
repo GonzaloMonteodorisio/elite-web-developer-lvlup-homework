41c329d094879b1d29ef7a657cbc44e449240001
GonzaloMonteodorisio <gonzalomonteodorisio@gmail.com>
2024-03-17 10:01:25 -0300

    update generics and index.ts

--- class_02/typescript-webpack/src/generics/index.ts
+++ class_02/typescript-webpack/src/generics/index.ts
@@ -1,3 +1,7 @@
 export const concatArrays = <T,U>(arr1: T[], arr2: U[]): (T | U)[] => {
   return [...arr1, ...arr2];
 }
+
+export const customMap = <T>(array:T[], fn: (element: T) => T): T[] => {
+  return array.map(fn);
+}
--- class_02/typescript-webpack/src/index.ts
+++ class_02/typescript-webpack/src/index.ts
@@ -1,5 +1,6 @@
 import { getpokemon } from "./utils/getPokemon";
 import { Pokemon } from "./types/pokemon";
+import { customMap } from "./generics";
 
 async function pokemon() {
   const pokemon = await getpokemon<Pokemon>(1);
@@ -7,3 +8,9 @@ async function pokemon() {
 }
 
 pokemon();
+
+const numbers: number[] = [1, 2, 3];
+
+const duplicateNumbers = customMap<number>(numbers, (x: number) => x *2);
+
+console.info('duplicateNumbers: ', duplicateNumbers);
